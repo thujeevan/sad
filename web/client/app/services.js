81881a1c763c79db63cfc13605ef1b292220a881
thujeevan <thujee@gmail.com>
2015-06-03 23:29:04 +0530

    moved authentication check to user service

--- web/client/app/services.js
+++ web/client/app/services.js
@@ -1,66 +1,79 @@
 'use strict';
 
-angular.module('clientApp.services', []).
-factory('userService', function() {
+angular.module('clientApp.services', []).factory('userService', function($q, $http) {
     var username = '';
 
     return {
-      username : username
+        isAuthenticated: function() {
+            var deferred = $q.defer();
+            var _this = this;
+            if (this.username) {
+                deferred.resolve(this.username);
+            } else {
+                $http.get('/session/isauthenticated')
+                        .success(function(result) {
+                            _this.username = result.data.username;
+                            deferred.resolve(result);
+                        })
+                        .error(function(result) {
+                            deferred.reject(result);
+                        });
+            }
+
+            return deferred.promise;
+        },
+        username: username
     };
-}).
-factory('alertService', function($timeout) {
-
-      var ALERT_TIMEOUT = 5000;
-
-      function add(type, msg, timeout) {
-
+}).factory('alertService', function($timeout) {
+    var ALERT_TIMEOUT = 5000;
+    
+    function add(type, msg, timeout) {
         if (timeout) {
-          $timeout(function(){
-            closeAlert(this);
-          }, timeout);
+            $timeout(function() {
+                closeAlert(this);
+            }, timeout);
         } else {
-          $timeout(function(){
-            closeAlert(this);
-          }, ALERT_TIMEOUT);
+            $timeout(function() {
+                closeAlert(this);
+            }, ALERT_TIMEOUT);
         }
 
         return alerts.push({
-          type: type,
-          msg: msg,
-          close: function() {
-            return closeAlert(this);
-          }
+            type: type,
+            msg: msg,
+            close: function() {
+                return closeAlert(this);
+            }
         });
-      }
-
-      function closeAlert(alert) {
-        return closeAlertIdx(alerts.indexOf(alert));
-      }
-
-      function closeAlertIdx(index) {
-        return alerts.splice(index, 1);
-      }
-
-      function clear(){
-        alerts = [];
-      }
-
-      function get() {
-        return alerts;
-      }
-
-      var service = {
-            add: add,
-            closeAlert: closeAlert,
-            closeAlertIdx: closeAlertIdx,
-            clear: clear,
-            get: get
-          },
-          alerts = [];
-
-      return service;
     }
-).
-controller('AlertsCtrl', function ($scope, alertService) {
+
+    function closeAlert(alert) {
+        return closeAlertIdx(alerts.indexOf(alert));
+    }
+
+    function closeAlertIdx(index) {
+        return alerts.splice(index, 1);
+    }
+
+    function clear() {
+        alerts = [];
+    }
+
+    function get() {
+        return alerts;
+    }
+
+    var service = {
+        add: add,
+        closeAlert: closeAlert,
+        closeAlertIdx: closeAlertIdx,
+        clear: clear,
+        get: get
+    },
+    alerts = [];
+
+    return service;
+}
+).controller('AlertsCtrl', function($scope, alertService) {
     $scope.alerts = alertService.get();
 });
